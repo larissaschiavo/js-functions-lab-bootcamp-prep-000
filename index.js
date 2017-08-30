5f3ee97d6d7c94e3bfc3ec83e0dbae3df77392d0
Larissa Schiavo <larissafschiavo@gmail.com>
2017-08-30 03:19:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function happyHolidaysTo(name) {
 
 function happyHolidayTo(holiday, name) {
   var message = `Happy ${holiday}, ${name}!`
-  return message;
+  return message
 }
 
 function holidayCountdown(holiday, days) {
